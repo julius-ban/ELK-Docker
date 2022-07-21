559a4651855468126b75b200bcad5724d4de7f8f
ysban <covj12@gmail.com>
2022-07-21 13:33:27 +0900

    modify

--- web/back/back_lib/connect_db.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-def connect_mongo():
-    client = MongoClient("localhost",27017)
-    return client
--- web/front/src/views/auth/RegisterForm.vue
+++
@@ -1,9 +0,0 @@
-<template> </template>
-
-<script>
-export default {
-    name: 'RegisterForm.vue',
-};
-</script>
-
-<style scoped></style>
